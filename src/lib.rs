a795c32ad2e533d37faa5656be9cf84f403d6e3c
Lingxuan Ye <love@lingxuan.io>
2024-04-18 20:56:06 +0800

    feat: implement constructor methods for `Matrix`

--- src/lib.rs
+++ src/lib.rs
@@ -1,10 +1,36 @@
 pub mod error;
 pub mod shape;
 
+use error::Result;
 pub use shape::Shape;
+use shape::TryIntoShape;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Matrix<T> {
     shape: Shape,
     data: Vec<T>,
+}
+
+impl<T: Clone + Default> Matrix<T> {
+    pub fn build<S: TryIntoShape>(shape: S) -> Result<Self> {
+        let shape = shape.try_into_shape()?;
+        let data = vec![Default::default(); shape.size()];
+        Ok(Self { shape, data })
+    }
+
+    pub fn new<S: TryIntoShape>(shape: S) -> Self {
+        match Self::build(shape) {
+            Ok(matrix) => matrix,
+            Err(error) => panic!("{error}"),
+        }
+    }
+}
+
+impl<T: Clone> Matrix<T> {
+    pub fn from_slice(src: &[T]) -> Self {
+        Self {
+            shape: Shape::build(1, src.len()).expect("this will never fail"),
+            data: src.to_vec(),
+        }
+    }
 }
