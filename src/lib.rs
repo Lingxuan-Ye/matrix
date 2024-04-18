36908d3e5d532f6e08d21e2a2e7c54389a5de409
Lingxuan Ye <love@lingxuan.io>
2024-04-18 20:57:23 +0800

    feat: implement property getters for `Matrix`

--- src/lib.rs
+++ src/lib.rs
@@ -34,3 +34,21 @@ impl<T: Clone> Matrix<T> {
         }
     }
 }
+
+impl<T> Matrix<T> {
+    pub fn shape(&self) -> &Shape {
+        &self.shape
+    }
+
+    pub fn nrows(&self) -> usize {
+        self.shape.nrows()
+    }
+
+    pub fn ncols(&self) -> usize {
+        self.shape.ncols()
+    }
+
+    pub fn size(&self) -> usize {
+        self.shape.size()
+    }
+}
