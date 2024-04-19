5ce64b9cdd48ee1e793637630822e761268af5c7
Lingxuan Ye <love@lingxuan.io>
2024-04-19 20:09:40 +0800

    feat: implement `std::ops::Index` and `std::ops::IndexMut` for `Matrix`

--- src/index.rs
+++ src/index.rs
@@ -1,3 +1,6 @@
+use crate::error::Error;
+use crate::Matrix;
+
 #[derive(Clone, Debug, PartialEq, Eq)]
 pub struct Index {
     pub row: usize,
@@ -21,5 +24,37 @@ impl From<[usize; 2]> for Index {
     fn from(value: [usize; 2]) -> Self {
         let [row, col] = value;
         Self { row, col }
+    }
+}
+
+impl<T> Matrix<T> {
+    fn flatten_index<I: Into<Index>>(&self, index: I) -> usize {
+        let index: Index = index.into();
+        if index.row >= self.nrows() || index.col >= self.ncols() {
+            panic!("{}", Error::IndexOutOfBounds);
+        }
+        index.row * self.ncols() + index.col
+    }
+}
+
+impl<T, I> std::ops::Index<I> for Matrix<T>
+where
+    I: Into<Index>,
+{
+    type Output = T;
+
+    fn index(&self, index: I) -> &Self::Output {
+        let index = self.flatten_index(index);
+        &self.data[index]
+    }
+}
+
+impl<T, I> std::ops::IndexMut<I> for Matrix<T>
+where
+    I: Into<Index>,
+{
+    fn index_mut(&mut self, index: I) -> &mut Self::Output {
+        let index = self.flatten_index(index);
+        &mut self.data[index]
     }
 }
