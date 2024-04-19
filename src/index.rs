ad247aeea218c6ebdfb868ccf36f700ef3d462e8
Lingxuan Ye <love@lingxuan.io>
2024-04-20 03:06:55 +0800

    refactor: change `Index` to tuple struct

--- src/index.rs
+++ src/index.rs
@@ -2,38 +2,45 @@ use crate::error::Error;
 use crate::Matrix;
 
 #[derive(Clone, Debug, PartialEq, Eq)]
-pub struct Index {
-    pub row: usize,
-    pub col: usize,
+pub struct Index(pub usize, pub usize);
+
+impl Index {
+    pub fn row(&self) -> usize {
+        self.0
+    }
+
+    pub fn col(&self) -> usize {
+        self.1
+    }
 }
 
 impl std::fmt::Display for Index {
     fn fmt(&self, f: &mut std::fmt::Formatter<'_>) -> std::fmt::Result {
-        write!(f, "({}, {})", self.row, self.col)
+        write!(f, "({}, {})", self.row(), self.col())
     }
 }
 
 impl From<(usize, usize)> for Index {
     fn from(value: (usize, usize)) -> Self {
         let (row, col) = value;
-        Self { row, col }
+        Self(row, col)
     }
 }
 
 impl From<[usize; 2]> for Index {
     fn from(value: [usize; 2]) -> Self {
         let [row, col] = value;
-        Self { row, col }
+        Self(row, col)
     }
 }
 
 impl<T> Matrix<T> {
     fn flatten_index<I: Into<Index>>(&self, index: I) -> usize {
         let index: Index = index.into();
-        if index.row >= self.nrows() || index.col >= self.ncols() {
+        if index.row() >= self.nrows() || index.col() >= self.ncols() {
             panic!("{}", Error::IndexOutOfBounds);
         }
-        index.row * self.ncols() + index.col
+        index.row() * self.ncols() + index.col()
     }
 }
 
