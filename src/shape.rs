8fffbc72a5f2e1579c7b9e3399de38bad085468c
Lingxuan Ye <love@lingxuan.io>
2024-04-20 03:21:29 +0800

    test: rewrite tests based on refactoring

--- src/shape.rs
+++ src/shape.rs
@@ -77,9 +77,9 @@ mod test {
 
     #[test]
     fn test_build() {
-        assert_eq!(Shape::build(0, 0).unwrap_err(), Error::ZeroSize);
-        assert_eq!(Shape::build(0, 1).unwrap_err(), Error::ZeroSize);
-        assert_eq!(Shape::build(1, 0).unwrap_err(), Error::ZeroSize);
+        let shape = Shape { nrows: 2, ncols: 3 };
+        assert_eq!(Shape::build(2, 3).unwrap(), shape);
+        assert_ne!(Shape::build(3, 2).unwrap(), shape);
 
         assert_eq!(
             Shape::build(usize::MAX, 2).unwrap_err(),
@@ -89,29 +89,21 @@ mod test {
             Shape::build(2, usize::MAX).unwrap_err(),
             Error::SizeOverflow
         );
-
-        let shape = Shape { nrows: 2, ncols: 3 };
-        assert_eq!(Shape::build(2, 3).unwrap(), shape);
-        assert_ne!(Shape::build(3, 2).unwrap(), shape);
+        assert_eq!(
+            Shape::build(usize::MAX, usize::MAX).unwrap_err(),
+            Error::SizeOverflow
+        );
     }
 
     #[test]
     fn test_display() {
-        assert_eq!(Shape::build(2, 3).unwrap().to_string(), "Shape(2, 3)");
-        assert_eq!(Shape::build(3, 2).unwrap().to_string(), "Shape(3, 2)");
+        assert_eq!(Shape::build(2, 3).unwrap().to_string(), "(2, 3)");
+        assert_eq!(Shape::build(3, 2).unwrap().to_string(), "(3, 2)");
     }
 
     #[test]
     fn test_try_into_shape() {
         let shape = Shape { nrows: 2, ncols: 3 };
-
-        assert_eq!((0, 1).try_into_shape().unwrap_err(), Error::ZeroSize);
-        assert_eq!((2, 3).try_into_shape().unwrap(), shape);
-        assert_ne!((3, 2).try_into_shape().unwrap(), shape);
-
-        assert_eq!([0, 1].try_into_shape().unwrap_err(), Error::ZeroSize);
-        assert_eq!([2, 3].try_into_shape().unwrap(), shape);
-        assert_ne!([3, 2].try_into_shape().unwrap(), shape);
 
         assert_eq!(
             Shape { nrows: 2, ncols: 3 }.try_into_shape().unwrap(),
@@ -120,6 +112,36 @@ mod test {
         assert_ne!(
             Shape { nrows: 3, ncols: 2 }.try_into_shape().unwrap(),
             shape
+        );
+
+        assert_eq!((2, 3).try_into_shape().unwrap(), shape);
+        assert_ne!((3, 2).try_into_shape().unwrap(), shape);
+        assert_eq!(
+            (usize::MAX, 2).try_into_shape().unwrap_err(),
+            Error::SizeOverflow
+        );
+        assert_eq!(
+            (2, usize::MAX).try_into_shape().unwrap_err(),
+            Error::SizeOverflow
+        );
+        assert_eq!(
+            (usize::MAX, usize::MAX).try_into_shape().unwrap_err(),
+            Error::SizeOverflow
+        );
+
+        assert_eq!([2, 3].try_into_shape().unwrap(), shape);
+        assert_ne!([3, 2].try_into_shape().unwrap(), shape);
+        assert_eq!(
+            [usize::MAX, 2].try_into_shape().unwrap_err(),
+            Error::SizeOverflow
+        );
+        assert_eq!(
+            [2, usize::MAX].try_into_shape().unwrap_err(),
+            Error::SizeOverflow
+        );
+        assert_eq!(
+            [usize::MAX, usize::MAX].try_into_shape().unwrap_err(),
+            Error::SizeOverflow
         );
     }
 }
