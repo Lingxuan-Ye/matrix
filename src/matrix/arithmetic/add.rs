ab238cbde104688e5703689dc3d9b250bc3bf824
Lingxuan Ye <love@lingxuan.io>
2024-05-17 04:41:47 +0800

    test: improve test coverage for mod `arithmetic`

--- src/matrix/arithmetic/add.rs
+++ src/matrix/arithmetic/add.rs
@@ -76,7 +76,7 @@ where
         let result =
             operation::elementwise_operation_assign_to_lhs(self, rhs, |(x, y)| *x += y.clone());
         if let Err(error) = result {
-            panic!("{error}")
+            panic!("{error}");
         }
     }
 }
@@ -90,7 +90,7 @@ where
         let result =
             operation::elementwise_operation_assign_to_lhs_consume_rhs(self, rhs, |(x, y)| *x += y);
         if let Err(error) = result {
-            panic!("{error}")
+            panic!("{error}");
         }
     }
 }
@@ -102,13 +102,11 @@ mod test {
     #[test]
     fn test_add() {
         let mut lhs = matrix![[0, 1, 2], [3, 4, 5]];
-        let mut rhs = matrix![[1, 1, 1], [2, 2, 2]];
-        let target = matrix![[1, 2, 3], [5, 6, 7]];
+        let mut rhs = matrix![[5, 4, 3], [2, 1, 0]];
+        let target = matrix![[5, 5, 5], [5, 5, 5]];
 
-        assert_eq!(&lhs + &rhs, target);
-        assert_eq!(&lhs + rhs.clone(), target);
-        assert_eq!(lhs.clone() + &rhs, target);
-        assert_eq!(lhs.clone() + rhs.clone(), target);
+        let result = &lhs + &rhs;
+        assert_eq!(result, target);
 
         rhs.switch_order();
         let result = &lhs + &rhs;
@@ -121,17 +119,9 @@ mod test {
         let mut result = &lhs + &rhs;
         assert_ne!(result, target);
         assert_eq!(result.order, lhs.order);
+        assert_ne!(result.order, rhs.order);
         result.switch_order();
         assert_eq!(result, target);
-        lhs.switch_order();
-
-        let mut lhs_clone = lhs.clone();
-        lhs_clone += &rhs;
-        assert_eq!(lhs_clone, target);
-
-        let mut lhs_clone = lhs.clone();
-        lhs_clone += rhs;
-        assert_eq!(lhs_clone, target);
     }
 
     #[test]
@@ -140,6 +130,182 @@ mod test {
         let lhs = matrix![[0, 1, 2], [3, 4, 5]];
         let rhs = matrix![[1, 1], [2, 2], [3, 3]];
 
+        let _ = &lhs + &rhs;
+    }
+
+    #[test]
+    fn test_add_consume_rhs() {
+        let mut lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let mut rhs = matrix![[5, 4, 3], [2, 1, 0]];
+        let target = matrix![[5, 5, 5], [5, 5, 5]];
+
+        let result = &lhs + rhs.clone();
+        assert_eq!(result, target);
+
+        rhs.switch_order();
+        let result = &lhs + rhs.clone();
+        assert_eq!(result, target);
+        assert_eq!(result.order, lhs.order);
+        assert_ne!(result.order, rhs.order);
+        rhs.switch_order();
+
+        lhs.switch_order();
+        let mut result = &lhs + rhs.clone();
+        assert_ne!(result, target);
+        assert_eq!(result.order, lhs.order);
+        assert_ne!(result.order, rhs.order);
+        result.switch_order();
+        assert_eq!(result, target);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_add_consume_rhs_fails() {
+        let lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let rhs = matrix![[1, 1], [2, 2], [3, 3]];
+
+        let _ = &lhs + rhs;
+    }
+
+    #[test]
+    fn test_add_consume_lhs() {
+        let mut lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let mut rhs = matrix![[5, 4, 3], [2, 1, 0]];
+        let target = matrix![[5, 5, 5], [5, 5, 5]];
+
+        let result = lhs.clone() + &rhs;
+        assert_eq!(result, target);
+
+        rhs.switch_order();
+        let result = lhs.clone() + &rhs;
+        assert_eq!(result, target);
+        assert_eq!(result.order, lhs.order);
+        assert_ne!(result.order, rhs.order);
+        rhs.switch_order();
+
+        lhs.switch_order();
+        let mut result = lhs.clone() + &rhs;
+        assert_ne!(result, target);
+        assert_eq!(result.order, lhs.order);
+        assert_ne!(result.order, rhs.order);
+        result.switch_order();
+        assert_eq!(result, target);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_add_consume_lhs_fails() {
+        let lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let rhs = matrix![[1, 1], [2, 2], [3, 3]];
+
+        let _ = lhs + &rhs;
+    }
+
+    #[test]
+    fn test_add_consume_both() {
+        let mut lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let mut rhs = matrix![[5, 4, 3], [2, 1, 0]];
+        let target = matrix![[5, 5, 5], [5, 5, 5]];
+
+        let result = lhs.clone() + rhs.clone();
+        assert_eq!(result, target);
+
+        rhs.switch_order();
+        let result = lhs.clone() + rhs.clone();
+        assert_eq!(result, target);
+        assert_eq!(result.order, lhs.order);
+        assert_ne!(result.order, rhs.order);
+        rhs.switch_order();
+
+        lhs.switch_order();
+        let mut result = lhs.clone() + rhs.clone();
+        assert_ne!(result, target);
+        assert_eq!(result.order, lhs.order);
+        assert_ne!(result.order, rhs.order);
+        result.switch_order();
+        assert_eq!(result, target);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_add_consume_both_fails() {
+        let lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let rhs = matrix![[1, 1], [2, 2], [3, 3]];
+
         let _ = lhs + rhs;
+    }
+
+    #[test]
+    fn test_add_assign_to_lhs() {
+        let mut lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let mut rhs = matrix![[5, 4, 3], [2, 1, 0]];
+        let target = matrix![[5, 5, 5], [5, 5, 5]];
+
+        let mut result = lhs.clone();
+        result += &rhs;
+        assert_eq!(result, target);
+
+        rhs.switch_order();
+        let mut result = lhs.clone();
+        result += &rhs;
+        assert_eq!(result, target);
+        assert_eq!(result.order, lhs.order);
+        assert_ne!(result.order, rhs.order);
+        rhs.switch_order();
+
+        lhs.switch_order();
+        let mut result = lhs.clone();
+        result += &rhs;
+        assert_ne!(result, target);
+        assert_eq!(result.order, lhs.order);
+        assert_ne!(result.order, rhs.order);
+        result.switch_order();
+        assert_eq!(result, target);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_add_assign_to_lhs_fails() {
+        let mut lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let rhs = matrix![[1, 1], [2, 2], [3, 3]];
+
+        lhs += &rhs;
+    }
+
+    #[test]
+    fn test_add_assign_to_lhs_consume_rhs() {
+        let mut lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let mut rhs = matrix![[5, 4, 3], [2, 1, 0]];
+        let target = matrix![[5, 5, 5], [5, 5, 5]];
+
+        let mut result = lhs.clone();
+        result += rhs.clone();
+        assert_eq!(result, target);
+
+        rhs.switch_order();
+        let mut result = lhs.clone();
+        result += rhs.clone();
+        assert_eq!(result, target);
+        assert_eq!(result.order, lhs.order);
+        assert_ne!(result.order, rhs.order);
+        rhs.switch_order();
+
+        lhs.switch_order();
+        let mut result = lhs.clone();
+        result += rhs.clone();
+        assert_ne!(result, target);
+        assert_eq!(result.order, lhs.order);
+        assert_ne!(result.order, rhs.order);
+        result.switch_order();
+        assert_eq!(result, target);
+    }
+
+    #[test]
+    #[should_panic]
+    fn test_add_assign_to_lhs_consume_rhs_fails() {
+        let mut lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let rhs = matrix![[1, 1], [2, 2], [3, 3]];
+
+        lhs += rhs;
     }
 }
