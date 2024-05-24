e9918fadc68742b1bfd3bb26b8d72becfd6e8608
Lingxuan Ye <love@lingxuan.io>
2024-05-24 19:16:35 +0800

    refactor: implement `set_dim!` for formatting

--- src/consts.rs
+++ src/consts.rs
@@ -1,8 +1,6 @@
-pub(crate) const LEFT_DELIMITER: &str = "[";
-pub(crate) const RIGHT_DELIMITER: &str = "]";
-pub(crate) const COMMA: &str = ",";
-pub(crate) const SPACE: &str = " ";
-pub(crate) const SET_DIM: &str = "\u{001b}[2m";
-pub(crate) const UNSET_DIM: &str = "\u{001b}[22m";
-pub(crate) const TAB_LEN: usize = 4;
-pub(crate) const SEP_LEN: usize = 2;
+pub const LEFT_DELIMITER: &str = "[";
+pub const RIGHT_DELIMITER: &str = "]";
+pub const COMMA: &str = ",";
+pub const SPACE: &str = " ";
+pub const TAB_LEN: usize = 4;
+pub const SEP_LEN: usize = 2;
