bae2897a68a042d1a7c3e120979966eb4702af04
Lingxuan Ye <love@lingxuan.io>
2024-05-24 18:04:04 +0800

    feat: extend `matrix!` macro

--- src/macros.rs
+++ src/macros.rs
@@ -3,15 +3,26 @@
 /// # Examples
 ///
 /// ```
-/// use matreex::matrix;
+/// use matreex::{matrix, Matrix};
 ///
-/// let matrix = matrix![[0, 1, 2], [3, 4, 5]];
+/// let foo: Matrix<i32> = matrix![];
+/// let bar = matrix![[0; 3]; 2];
+/// let baz = matrix![[0, 1, 2], [3, 4, 5]];
 /// ```
 ///
 /// [`Matrix`]: crate::matrix::Matrix
 #[macro_export]
 macro_rules! matrix {
+    [] => {{
+        let shape = $crate::matrix::shape::Shape::new(0, 0);
+        $crate::matrix::Matrix::new(shape)
+    }};
+
+    [$elem:expr; $n:expr] => {
+        $crate::matrix::Matrix::from([$elem; $n])
+    };
+
     [$($col:expr),+ $(,)?] => {
-        $crate::matrix::Matrix::from(std::boxed::Box::new([$($col,)+]))
+        $crate::matrix::Matrix::from([$($col,)+])
     };
 }
