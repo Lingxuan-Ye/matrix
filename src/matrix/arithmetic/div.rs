585a0b9909f1b1d1fb75538ae83c1342fa5a4dbd
Lingxuan Ye <love@lingxuan.io>
2024-06-15 21:11:50 +0800

    test: add test cases

--- src/matrix/arithmetic/div.rs
+++ src/matrix/arithmetic/div.rs
@@ -99,131 +99,32 @@ impl_scalar_div! {u8 u16 u32 u64 u128 usize i8 i16 i32 i64 i128 isize f32 f64}
 
 #[cfg(test)]
 mod tests {
-    use super::super::super::Matrix;
     use crate::matrix;
 
     #[test]
-    #[allow(clippy::op_ref)]
-    fn test_matrix_div_scalar() {
-        let mut lhs = matrix![[0, 1, 2], [3, 4, 5]];
-        let rhs = 2;
-        let expected = matrix![[0, 0, 1], [1, 2, 2]];
+    fn test_elementwise_div() {
+        let lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let rhs = matrix![[2, 2, 2], [2, 2, 2]];
 
-        {
-            let result = &lhs / &rhs;
-            assert_eq!(result, expected);
-
-            let result = &lhs / rhs;
-            assert_eq!(result, expected);
-
-            let result = lhs.clone() / &rhs;
-            assert_eq!(result, expected);
-
-            let result = lhs.clone() / rhs;
-            assert_eq!(result, expected);
-        }
-
-        {
-            lhs.switch_order();
-
-            let mut result: Matrix<i32> = &lhs / &rhs;
-            assert_ne!(result, expected);
-            result.switch_order();
-            assert_eq!(result, expected);
-
-            let mut result: Matrix<i32> = &lhs / rhs;
-            assert_ne!(result, expected);
-            result.switch_order();
-            assert_eq!(result, expected);
-
-            let mut result: Matrix<i32> = lhs.clone() / &rhs;
-            assert_ne!(result, expected);
-            result.switch_order();
-            assert_eq!(result, expected);
-
-            let mut result: Matrix<i32> = lhs.clone() / rhs;
-            assert_ne!(result, expected);
-            result.switch_order();
-            assert_eq!(result, expected);
-        }
+        let result = lhs.elementwise_div(&rhs);
+        assert_eq!(result, Ok(matrix![[0, 0, 1], [1, 2, 2]]));
     }
 
     #[test]
-    #[allow(clippy::op_ref)]
-    fn test_scalar_div_matrix() {
-        let lhs = 12;
-        let mut rhs = matrix![[1, 2, 3], [4, 5, 6]];
-        let expected = matrix![[12, 6, 4], [3, 2, 2]];
+    fn test_elementwise_div_consume_self() {
+        let lhs = matrix![[0, 1, 2], [3, 4, 5]];
+        let rhs = matrix![[2, 2, 2], [2, 2, 2]];
 
-        {
-            let result = &lhs / &rhs;
-            assert_eq!(result, expected);
-
-            let result = lhs / &rhs;
-            assert_eq!(result, expected);
-
-            let result = &lhs / rhs.clone();
-            assert_eq!(result, expected);
-
-            let result = lhs / rhs.clone();
-            assert_eq!(result, expected);
-        }
-
-        {
-            rhs.switch_order();
-
-            let mut result: Matrix<i32> = &lhs / &rhs;
-            assert_ne!(result, expected);
-            result.switch_order();
-            assert_eq!(result, expected);
-
-            let mut result: Matrix<i32> = lhs / &rhs;
-            assert_ne!(result, expected);
-            result.switch_order();
-            assert_eq!(result, expected);
-
-            let mut result: Matrix<i32> = &lhs / rhs.clone();
-            assert_ne!(result, expected);
-            result.switch_order();
-            assert_eq!(result, expected);
-
-            let mut result: Matrix<i32> = lhs / rhs.clone();
-            assert_ne!(result, expected);
-            result.switch_order();
-            assert_eq!(result, expected);
-        }
+        let result = lhs.elementwise_div_consume_self(&rhs);
+        assert_eq!(result, Ok(matrix![[0, 0, 1], [1, 2, 2]]));
     }
 
     #[test]
-    fn test_matrix_div_scalar_assign() {
+    fn test_elementwise_div_assign() {
         let mut lhs = matrix![[0, 1, 2], [3, 4, 5]];
-        let rhs = 2;
-        let expected = matrix![[0, 0, 1], [1, 2, 2]];
+        let rhs = matrix![[2, 2, 2], [2, 2, 2]];
 
-        {
-            let mut result = lhs.clone();
-            result /= &rhs;
-            assert_eq!(result, expected);
-
-            let mut result = lhs.clone();
-            result /= rhs;
-            assert_eq!(result, expected);
-        }
-
-        {
-            lhs.switch_order();
-
-            let mut result = lhs.clone();
-            result /= &rhs;
-            assert_ne!(result, expected);
-            result.switch_order();
-            assert_eq!(result, expected);
-
-            let mut result = lhs.clone();
-            result /= rhs;
-            assert_ne!(result, expected);
-            result.switch_order();
-            assert_eq!(result, expected);
-        }
+        lhs.elementwise_div_assign(&rhs).unwrap();
+        assert_eq!(lhs, matrix![[0, 0, 1], [1, 2, 2]]);
     }
 }
