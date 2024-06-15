8dfc4122b0736620f241bd37223da776b0641fa8
Lingxuan Ye <love@lingxuan.io>
2024-06-15 23:02:29 +0800

    feat: implement `Neg` for `Matrix`

---
+++ src/matrix/arithmetic/neg.rs
@@ -0,0 +1,24 @@
+use super::super::Matrix;
+use std::ops::Neg;
+
+impl<T, U> Neg for Matrix<T>
+where
+    T: Neg<Output = U>,
+{
+    type Output = Matrix<U>;
+
+    fn neg(self) -> Self::Output {
+        self.map(|element| element.neg())
+    }
+}
+
+impl<T, U> Neg for &Matrix<T>
+where
+    T: Neg<Output = U> + Clone,
+{
+    type Output = Matrix<U>;
+
+    fn neg(self) -> Self::Output {
+        self.clone().neg()
+    }
+}
